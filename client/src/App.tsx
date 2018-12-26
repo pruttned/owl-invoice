2b702bc1af808e8d966749adabd749fe47a6bf9f
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-12-26 21:33:41 +0100

    feat(client): add routing and main layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,76 +1,91 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.scss';
-import Button from '@material-ui/core/Button';
-import ItemList from './common/item-list';
-import { MenuItem } from '@material-ui/core';
-import InvoiceList from './app/invoice/invoice-list';
-import { Invoice } from './app/invoice/invoice';
-import Decimal from 'decimal.js';
-import { Client } from './app/client/client';
-import ClientList from './app/client/client-list';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import InvoiceListPage from './app/invoice/pages/invoice-list-page';
+import ClientListPage from './app/client/pages/client-list-page';
+import { AppBar, List, ListItem, ListItemText, Drawer, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { Menu as MenuIcon } from '@material-ui/icons';
+import styles from './App.module.scss';
 
-const clients: Client[] = [
-  {
-    id: 'c1',
-    name: 'client 1',
-    color: '#00d8ff',
-    initials: 'C1',
-    address: 'address xxx',
-    taxId: 'dic1',
-    businessId: 'ico1',
-    vatNumber: 'icdph1'
-  },
-  {
-    id: 'c2',
-    name: 'client 2',
-    color: 'red',
-    initials: 'C2',
-    address: 'address xxx2',
-    taxId: 'dic2',
-    businessId: 'ico2',
-    vatNumber: 'icdph2'
-  }
-]
+const drawer = (
+  <List>
+    <Link to="/">
+      <ListItem button>
+        <ListItemText primary="Invoices" />
+      </ListItem>
+    </Link>
+    <Link to="/clients">
+      <ListItem button>
+        <ListItemText primary="Clients" />
+      </ListItem>
+    </Link>
+  </List>
+);
 
+interface AppState {
+  mobileOpen: boolean;
+}
 
-const invoices: Invoice[] = [
-  {
-    id: '1', issueDate: new Date(), dueDate: new Date(), number: '2018001', client: clients[0],
-    items: [{
-      text: 't',
-      unitCount: new Decimal(2),
-      unitPrice: new Decimal(3),
+class App extends Component<any, AppState> {
+  state = {
+    mobileOpen: false,
+  };
 
-    }]
-  },
-  {
-    id: '2', issueDate: new Date(), dueDate: new Date(), number: '2018002',
-    client: clients[1],
-    items: [{
-      text: 't',
-      unitCount: new Decimal(1),
-      unitPrice: new Decimal(10),
-    },
-    {
-      text: 't2',
-      unitCount: new Decimal(2),
-      unitPrice: new Decimal(1),
-    }]
-  },
-]
+  toggleDrawer = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <InvoiceList items={invoices} />
-          <br /><br />
-          <ClientList items={clients} />
+      <Router>
+        <div className={styles.root}>
+          <AppBar position="fixed" className={styles.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.toggleDrawer}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" noWrap>
+                Owl Invoice
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <nav>
+            <div className={styles.drawerMobile}>
+              <Drawer
+                className={styles.drawer}
+                variant="temporary"
+                anchor="left"
+                open={this.state.mobileOpen}
+                onClose={this.toggleDrawer}
+                classes={{
+                  paper: styles.drawerPaper,
+                }}
+              >
+                {drawer}
+              </Drawer>
+            </div>
+            <div className={styles.drawerDesktop}>
+              <Drawer
+                className={styles.drawer}
+                classes={{
+                  paper: styles.drawerPaper,
+                }}
+                variant="permanent"
+                open>
+                {drawer}
+              </Drawer>
+            </div>
+          </nav>
+          <div className={styles.mainContent}>
+            <Route path="/" exact component={InvoiceListPage} />
+            <Route path="/clients/" component={ClientListPage} />
+          </div>
         </div>
-      </div>
-
+      </Router >
     );
   }
 }
---
+++ client/src/app/invoice/pages/invoice-list-page.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Client } from '../../client/client';
+import { Invoice } from '../invoice';
+import Decimal from 'decimal.js';
+import InvoiceList from '../invoice-list';
+
+const clients: Client[] = [
+    {
+        id: 'c1',
+        name: 'client 1',
+        color: '#00d8ff',
+        initials: 'C1',
+        address: 'address xxx',
+        taxId: 'dic1',
+        businessId: 'ico1',
+        vatNumber: 'icdph1'
+    },
+    {
+        id: 'c2',
+        name: 'client 2',
+        color: 'red',
+        initials: 'C2',
+        address: 'address xxx2',
+        taxId: 'dic2',
+        businessId: 'ico2',
+        vatNumber: 'icdph2'
+    }
+]
+
+
+const invoices: Invoice[] = [
+    {
+        id: '1', issueDate: new Date(), dueDate: new Date(), number: '2018001', client: clients[0],
+        items: [{
+            text: 't',
+            unitCount: new Decimal(2),
+            unitPrice: new Decimal(3),
+
+        }]
+    },
+    {
+        id: '2', issueDate: new Date(), dueDate: new Date(), number: '2018002',
+        client: clients[1],
+        items: [{
+            text: 't',
+            unitCount: new Decimal(1),
+            unitPrice: new Decimal(10),
+        },
+        {
+            text: 't2',
+            unitCount: new Decimal(2),
+            unitPrice: new Decimal(1),
+        }]
+    },
+]
+
+class InvoiceListPage extends Component {
+    render() {
+        return (
+            <InvoiceList items={invoices} />
+        );
+    }
+}
+
+export default InvoiceListPage;
