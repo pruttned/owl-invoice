b64bad9e81b2b8997e72114e7225bae38a4e9bf9
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-12-26 22:21:43 +0100

    feat(app): set theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,20 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import InvoiceListPage from './app/invoice/pages/invoice-list-page';
 import ClientListPage from './app/client/pages/client-list-page';
-import { AppBar, List, ListItem, ListItemText, Drawer, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { AppBar, List, ListItem, ListItemText, Drawer, Toolbar, IconButton, Typography, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import { Menu as MenuIcon } from '@material-ui/icons';
 import styles from './App.module.scss';
 
-
-interface AppState {
-  mobileOpen: boolean;
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00897b',
+    },
+    secondary: {
+      main: '#ffa726',
+    },
+  },
+});
 
 //TODO: extract
 const drawer = (
@@ -28,6 +34,10 @@ const drawer = (
   </List>
 );
 
+interface AppState {
+  mobileOpen: boolean;
+}
+
 class App extends Component<any, AppState> {
   state = {
     mobileOpen: false,
@@ -39,57 +49,59 @@ class App extends Component<any, AppState> {
 
   render() {
     return (
-      <Router>
-        <div className={styles.root}>
-          <AppBar position="fixed" className={styles.appBar}>
-            <Toolbar>
-              <IconButton
-                className={styles.menuButton}
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={this.toggleDrawer}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" noWrap>
-                Owl Invoice
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className={styles.root}>
+            <AppBar position="fixed" className={styles.appBar}>
+              <Toolbar>
+                <IconButton
+                  className={styles.menuButton}
+                  color="inherit"
+                  aria-label="Open drawer"
+                  onClick={this.toggleDrawer}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Owl Invoice
               </Typography>
-            </Toolbar>
-          </AppBar>
-          <nav>
-            <div className={styles.drawerMobile}>
-              <Drawer
-                className={styles.drawer}
-                variant="temporary"
-                anchor="left"
-                open={this.state.mobileOpen}
-                onClose={this.toggleDrawer}
-                classes={{
-                  paper: styles.drawerPaper,
-                }}
-              >
-                {drawer}
-              </Drawer>
+              </Toolbar>
+            </AppBar>
+            <nav>
+              <div className={styles.drawerMobile}>
+                <Drawer
+                  className={styles.drawer}
+                  variant="temporary"
+                  anchor="left"
+                  open={this.state.mobileOpen}
+                  onClose={this.toggleDrawer}
+                  classes={{
+                    paper: styles.drawerPaper,
+                  }}
+                >
+                  {drawer}
+                </Drawer>
+              </div>
+              <div className={styles.drawerDesktop}>
+                <Drawer
+                  className={styles.drawer}
+                  classes={{
+                    paper: styles.drawerPaper,
+                  }}
+                  variant="permanent"
+                  open>
+                  {drawer}
+                </Drawer>
+              </div>
+            </nav>
+            <div className={styles.mainContent}>
+              {/* TODO: extract */}
+              <Route path="/" exact component={InvoiceListPage} />
+              <Route path="/clients/" component={ClientListPage} />
             </div>
-            <div className={styles.drawerDesktop}>
-              <Drawer
-                className={styles.drawer}
-                classes={{
-                  paper: styles.drawerPaper,
-                }}
-                variant="permanent"
-                open>
-                {drawer}
-              </Drawer>
-            </div>
-          </nav>
-          <div className={styles.mainContent}>
-            {/* TODO: extract */}
-            <Route path="/" exact component={InvoiceListPage} />
-            <Route path="/clients/" component={ClientListPage} />
           </div>
-        </div>
-      </Router >
+        </Router >
+      </MuiThemeProvider>
     );
   }
 }
