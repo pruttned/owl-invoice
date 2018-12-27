5a1f5b4098a38523208960c0f5824815a71e6647
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-12-27 19:43:53 +0100

    feat(clientList): add data load

--- client/src/app/client/pages/client-list-page.tsx
+++ client/src/app/client/pages/client-list-page.tsx
@@ -1,36 +1,28 @@
 import React, { Component } from 'react';
 import { Client } from '../client';
 import ClientList from '../client-list/client-list';
+import gql from 'graphql-tag';
+import QueryPanel from '../../../common/query/query-panel';
 
-const clients: Client[] = [
-    {
-        id: 'c1',
-        name: 'client 1',
-        color: '#00d8ff',
-        initials: 'C1',
-        address: 'address xxx',
-        taxId: 'dic1',
-        businessId: 'ico1',
-        vatNumber: 'icdph1'
-    },
-    {
-        id: 'c2',
-        name: 'client 2',
-        color: 'red',
-        initials: 'C2',
-        address: 'address xxx2',
-        taxId: 'dic2',
-        businessId: 'ico2',
-        vatNumber: 'icdph2'
+const CLIENT_LIST_QUERY = gql`
+    query listClients {
+        clients {
+            id
+            name
+            color
+            initials
+        }
     }
-]
+`;
 
-class ClientListPage extends Component {
-    render() {
-        return (
-            <ClientList items={clients} />
-        );
-    }
-}
+interface Response {
+    clients: Client[];
+};
 
-export default ClientListPage;
+export default () => (
+    <QueryPanel<Response> query={CLIENT_LIST_QUERY}>
+        {(data) => {
+            return <ClientList items={data.clients} />
+        }}
+    </QueryPanel>
+)
