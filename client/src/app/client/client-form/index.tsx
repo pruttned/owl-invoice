e7b391cb0a9fce512e84ee1aacf06febde1b971d
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-12-27 12:33:02 +0100

    feat(client): add bae for edit page form

--- client/src/app/client/client-form/index.tsx
+++ client/src/app/client/client-form/index.tsx
@@ -1,93 +1,48 @@
 import React, { Component } from 'react';
-import ItemList from '../../../common/item-list';
 import { TextField } from '@material-ui/core';
-import styles from './client-form.module.scss';
 import { Client } from '../client';
-import Avatar from '../../../common/avatar/avatar';
+import { object as yupObject, string as yupString } from 'yup';
+import FormTextField from '../../../common/form/form-text-field';
+import Form from '../../../common/form/form';
 
 interface ClientFormProps {
-    client: Client
+    client: any
 }
 
 // interface ClientFormState {
 //     client: Client
 // }
 
+const validationSchema = yupObject().shape({
+    name: yupString()
+        .required('name is required!'),
+    address: yupString()
+        .required('address is required!'),
+})
+
+
 class ClientForm extends Component<ClientFormProps, Client> {
     constructor(props: ClientFormProps) {
         super(props);
         this.state = { ...props.client };
-
-        this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event: any) {
-              const target = event.target;
-              const value = target.value;
-              const name = target.name;
-
-            //   this.setState({
-            //     [name]: value
-            //   });
     }
 
     render() {
         return (
-            <form>
-                <TextField label="Name" value={this.state.name} onChange={this.handleChange} />
-                <TextField label="Address" value={this.state.address} onChange={this.handleChange} />
-            </form>
+            <Form
+                initialValues={this.props.client}
+                validationSchema={validationSchema}
+                submitText="Save"
+            >
+                {() => (
+                    <React.Fragment>
+                        <FormTextField name="name" label="Name" />
+                        <FormTextField name="address" label="Address" />
+                    </React.Fragment>
+                )}
+            </Form>
         );
     }
 }
 
 export default ClientForm;
-
-
-
-// class Reservation extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         isGoing: true,
-//         numberOfGuests: 2
-//       };
-
-//       this.handleInputChange = this.handleInputChange.bind(this);
-//     }
-
-//     handleInputChange(event) {
-//       const target = event.target;
-//       const value = target.type === 'checkbox' ? target.checked : target.value;
-//       const name = target.name;
-
-//       this.setState({
-//         [name]: value
-//       });
-//     }
-
-//     render() {
-//       return (
-//         <form>
-//           <label>
-//             Is going:
-//             <input
-//               name="isGoing"
-//               type="checkbox"
-//               checked={this.state.isGoing}
-//               onChange={this.handleInputChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Number of guests:
-//             <input
-//               name="numberOfGuests"
-//               type="number"
-//               value={this.state.numberOfGuests}
-//               onChange={this.handleInputChange} />
-//           </label>
-//         </form>
-//       );
-//     }
-//   }
--- client/src/app/client/pages/client-edit-page.tsx
+++ client/src/app/client/pages/client-edit-page.tsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { Client } from '../client';
 import ClientList from '../client-list';
+import ClientForm from '../client-form';
 
-// const client: Client = [
-//     {
-//         id: 'c1',
-//         name: 'client 1',
-//         color: '#00d8ff',
-//         initials: 'C1',
-//         address: 'address xxx',
-//         taxId: 'dic1',
-//         businessId: 'ico1',
-//         vatNumber: 'icdph1'
-//     }
-// ]
+const client: Client =
+{
+    id: 'c1',
+    name: 'client 1',
+    color: '#00d8ff',
+    initials: 'C1',
+    address: 'address xxx',
+    taxId: 'dic1',
+    businessId: 'ico1',
+    vatNumber: 'icdph1'
+};
+
 
 class ClientEditPage extends Component {
     render() {
         return (
-           <div>hello</div>
+            <ClientForm client={client} />
         );
     }
 }
