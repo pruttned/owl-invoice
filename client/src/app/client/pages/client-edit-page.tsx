ea48226ab8ffde19c9ce18d8daaa9e04d2cf8e98
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2019-01-09 21:41:26 +0100

    feat(clientEditPage): add redirect on success

--- client/src/app/client/pages/client-edit-page.tsx
+++ client/src/app/client/pages/client-edit-page.tsx
@@ -3,6 +3,7 @@ import { Client } from '../client';
 import ClientForm from '../client-form/client-form';
 import gql from 'graphql-tag';
 import QueryPanel from '../../../common/query/query-panel';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 const CLIENT_GET_QUERY = gql`
     query getClient($id: String!) {
@@ -40,12 +41,32 @@ interface Response {
     client: Client;
 };
 
-const ClientEditPage = ({ match }: { match: any }) => (
-    <QueryPanel<Response> query={CLIENT_GET_QUERY} variables={{ id: match.params.id }}>
-        {(data) => {
-            return <ClientForm client={data.client} mutation={CLIENT_UPDATE_QUERY} successMessage = "Client has been successfully updated"/>
-        }}
-    </QueryPanel>
-)
+interface ClientCreatePageProps extends RouteComponentProps<any> {
+}
 
-export default ClientEditPage;
+
+class ClientEditPage extends Component<ClientCreatePageProps> {
+
+    onSuccess = (resp: any) => {
+        this.props.history.push('/clients');
+    };
+
+    render() {
+        return (
+            <QueryPanel<Response> query={CLIENT_GET_QUERY} variables={{ id: this.props.match.params.id }}>
+                {(data) =>
+                    (
+                        <ClientForm
+                            client={data.client}
+                            mutation={CLIENT_UPDATE_QUERY}
+                            successMessage="Client has been successfully updated"
+                            onSuccess={this.onSuccess}
+                        />
+                    )
+                }
+            </QueryPanel>
+        );
+    }
+};
+
+export default withRouter(ClientEditPage);
