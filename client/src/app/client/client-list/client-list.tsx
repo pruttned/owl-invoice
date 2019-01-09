52b346680b8b51025c7974c25622215fec88c45c
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2019-01-09 22:00:17 +0100

    refactor(clientList): extract add button to page

--- client/src/app/client/client-list/client-list.tsx
+++ client/src/app/client/client-list/client-list.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import ItemList from '../../../common/item-list/item-list';
-import { MenuItem, Fab } from '@material-ui/core';
+import { MenuItem} from '@material-ui/core';
 import styles from './client-list.module.scss';
 import { Client } from '../client';
 import Avatar from '../../../common/avatar/avatar';
-import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
-import { Add as AddIcon } from '@material-ui/icons';
 
-interface ClientListProps extends RouteComponentProps<any> {
+interface ClientListProps {
     items: Client[]
 }
 
@@ -28,34 +26,23 @@ const AvatarColumn = ({ client }: { client: Client }) => {
 };
 
 class ClientList extends Component<ClientListProps> {
-    navigateToAdd = () => {
-        this.props.history.push('/clients/new');
-    };
-
     render() {
         return (
-            <React.Fragment>
-                <ItemList<Client> items={this.props.items}
-                    itemUrl={(item: Client) => `/clients/${encodeURIComponent(item.id)}`}
-                    itemRender={(item: Client) => (
-                        <div className={styles.item}>
-                            <AvatarColumn client={item} />
-                            <ClientColumn client={item} />
-                        </div>
-                    )}
-                    menuRender={(item: any, closeMenu: () => void) => [
-                        <MenuItem key="m1" onClick={() => { console.log(item); closeMenu(); }}>action1 {item.name}</MenuItem>,
-                        <MenuItem key="m2" onClick={() => { console.log(item); closeMenu(); }}>action2 {item.name}</MenuItem>,
-                    ]}
-                />
-                <aside className={styles.addButton}>
-                    <Fab color='primary' onClick={this.navigateToAdd}>
-                        <AddIcon />
-                    </Fab>
-                </aside>
-            </React.Fragment>
+            <ItemList<Client> items={this.props.items}
+                itemUrl={(item: Client) => `/clients/${encodeURIComponent(item.id)}`}
+                itemRender={(item: Client) => (
+                    <div className={styles.item}>
+                        <AvatarColumn client={item} />
+                        <ClientColumn client={item} />
+                    </div>
+                )}
+                menuRender={(item: any, closeMenu: () => void) => [
+                    <MenuItem key="m1" onClick={() => { console.log(item); closeMenu(); }}>action1 {item.name}</MenuItem>,
+                    <MenuItem key="m2" onClick={() => { console.log(item); closeMenu(); }}>action2 {item.name}</MenuItem>,
+                ]}
+            />
         );
     }
 }
 
-export default withRouter(ClientList);
+export default ClientList;
--- client/src/app/client/pages/client-list-page.tsx
+++ client/src/app/client/pages/client-list-page.tsx
@@ -3,6 +3,9 @@ import { Client } from '../client';
 import ClientList from '../client-list/client-list';
 import gql from 'graphql-tag';
 import QueryPanel from '../../../common/query/query-panel';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { Add as AddIcon } from '@material-ui/icons';
+import PageFab from '../../../common/page-fab/page-fab';
 
 const CLIENT_LIST_QUERY = gql`
     query listClients {
@@ -19,12 +22,29 @@ interface Response {
     clients: Client[];
 };
 
-const ClientListPage = () => (
-    <QueryPanel<Response> query={CLIENT_LIST_QUERY}>
-        {(data) => {
-            return <ClientList items={data.clients} />
-        }}
-    </QueryPanel>
-)
+interface ClientListPageProps extends RouteComponentProps<any> {
+}
 
-export default ClientListPage;
+class ClientListPage extends Component<ClientListPageProps>{
+    navigateToAdd = () => {
+        this.props.history.push('/clients/new');
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <QueryPanel<Response> query={CLIENT_LIST_QUERY}>
+                    {(data) => {
+                        return <ClientList items={data.clients} />
+                    }}
+                </QueryPanel>
+                <PageFab onClick={this.navigateToAdd}>
+                    <AddIcon />
+                </PageFab>
+            </React.Fragment>
+        );
+    }
+
+}
+
+export default withRouter(ClientListPage);
