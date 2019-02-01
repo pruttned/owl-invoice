8a9503a6103b6ff472b9eccb33553259903283e5
Ivan Ruttkay-Nedecky <iruttkayn@gmail.com>
2019-02-01 22:06:35 +0100

    feat(InvoiceCreatePage): add

--- client/src/app-routes.tsx
+++ client/src/app-routes.tsx
@@ -4,6 +4,7 @@ import InvoiceListPage from './app/invoice/pages/invoice-list-page';
 import ClientListPage from './app/client/pages/client-list-page';
 import ClientEditPage from './app/client/pages/client-edit-page';
 import ClientCreatePage from './app/client/pages/client-create-page';
+import InvoiceCreatePage from './app/invoice/pages/invoice-create-page';
 
 const AppRoutes = () => {
     return (
@@ -13,6 +14,10 @@ const AppRoutes = () => {
                 <Route path="/clients/" exact component={ClientListPage} />
                 <Route path="/clients/new" exact component={ClientCreatePage} />
                 <Route path="/clients/:id" exact component={ClientEditPage} />
+
+                <Route path="/invoices/" exact component={InvoiceListPage} />
+                <Route path="/invoices/new" exact component={InvoiceCreatePage} />
+                {/* <Route path="/invoices/:id" exact component={InvoiceEditPage} /> */}
             </Switch>
         </React.Fragment>
     );
---
+++ client/src/app/invoice/pages/invoice-create-page.tsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import InvoiceForm from '../invoice-form/invoice-form';
+import Decimal from 'decimal.js';
+import QueryPanel from '../../../common/query/query-panel';
+import { Client } from '../../client/client';
+
+const CLIENT_LIST_QUERY = gql`
+    query listClients {
+        clients {
+            id
+            name
+        }
+    }
+`;
+
+const INVOICE_CREATE_QUERY = gql`
+mutation createInvoice($model:CreateInvoiceInput!) {
+    createInvoice(input: $model) {
+        id
+        issueDate
+        dueDate
+        client {
+            name
+            color
+            initials
+            address
+            taxId
+            businessId
+            vatNumber
+        }
+        items {
+            text
+            unitCount
+            unitPrice
+        }
+    }
+  } 
+`;
+
+interface Response {
+    clients: Client[];
+};
+
+interface InvoiceCreatePageProps extends RouteComponentProps<any> {
+}
+
+class InvoiceCreatePage extends Component<InvoiceCreatePageProps> {
+
+    onSuccess = (resp: any) => {
+        this.props.history.push('/invoices');
+    };
+
+    getDefaultDueDate = () => {
+        let date = new Date();
+        date.setDate(date.getDate() + 20);
+        return date;
+    };
+
+    render() {
+        return (
+
+            <QueryPanel<Response> query={CLIENT_LIST_QUERY}>
+                {(data) =>
+                    (
+                        <InvoiceForm
+                            invoice={{
+                                dueDate: this.getDefaultDueDate(),
+                                issueDate: new Date(),
+                                client: data.clients[0].id,
+                                items: [{ text: '', unitCount: new Decimal(1), unitPrice: new Decimal(1) }],
+                            }}
+                            clients={data.clients}
+                            mutation={INVOICE_CREATE_QUERY}
+                            successMessage="Inivoice has been successfully created"
+                            onSuccess={this.onSuccess}
+                            invalidateQueryCache={true}
+                        />)
+                }
+            </QueryPanel>
+        );
+    }
+};
+
+export default withRouter(InvoiceCreatePage);
