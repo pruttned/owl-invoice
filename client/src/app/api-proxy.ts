6a4e764370bb65e9137df097eada4f71bb111cfb
Ivan Ruttkay-Nedecky <iruttkayn@gmail.com>
2019-02-06 23:03:03 +0100

    fix(ApiProxy): use relative uri

--- client/src/app/api-proxy.ts
+++ client/src/app/api-proxy.ts
@@ -1,5 +1,5 @@
 class ApiProxy {
-    private apiBaseUrl = 'http://localhost:3001/api';
+    private apiBaseUrl = 'api';
 
     exportInvoice(invoiceId: string, templateDefinition: InvoiceTemplateDefinition) {
         window.open(`${this.apiBaseUrl}/invoices/export?invoiceId=${encodeURIComponent(invoiceId)}&templateDefinitionId=${templateDefinition}`);
