1e3a5c0e9129468b95795657d03dc2603584c929
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2019-04-01 21:52:22 +0200

    feat(client): add commit and push to git repo

--- src/app/client/client-service.ts
+++ src/app/client/client-service.ts
@@ -3,6 +3,7 @@ import { InvoiceDocument } from './../invoice/invoice-document';
 import { ClientDocument } from './client-document';
 import { db } from '../db';
 import { slugify } from '../../common/slugify';
+import { repoService } from '../repo/repo-service';
 
 class ClientService {
     getAll(): Promise<ClientDocument[]> {
@@ -11,12 +12,16 @@ class ClientService {
     getById(id: string): Promise<ClientDocument> {
         return db.clients.single(id);
     }
-    create(client: ClientDocument): Promise<ClientDocument> {
+    async create(client: ClientDocument): Promise<ClientDocument> {
         client = { ...client, id: slugify(client.name) };
-        return db.clients.create(client);
+        await db.clients.create(client);
+        await repoService.commitAndPush(`client(${client.id}):created`);
+        return client;
     }
-    update(client: ClientDocument): Promise<ClientDocument> {
-        return db.clients.update(client);
+    async update(client: ClientDocument): Promise<ClientDocument> {
+        let clientDocument = await db.clients.update(client);
+        await repoService.commitAndPush(`client(${clientDocument.id}):updated`);
+        return clientDocument;
     }
     async remove(id: string): Promise<any> {
         let clientInvoices = await db.invoices.getAll({
@@ -28,7 +33,8 @@ class ClientService {
         if (clientInvoices.length) {
             throw new ConflictError('It is not allowed to remove client with existing invoices');
         }
-        return db.clients.remove(id);
+        await db.clients.remove(id);
+        await repoService.commitAndPush(`client(${id}):removed`);
     }
 }
 
